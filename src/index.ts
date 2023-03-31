8acf907bf8f123e6cbcbbb2a6f73f0284528c8fd
Sasha Karpovich <roobel2005@gmail.com>
2023-03-31 23:38:51 +0300

    update interface

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,7 @@ bot.start(async (ctx) => {
   await ctx.replyWithPhoto(
     'https://avatars.mds.yandex.net/get-marketpic/8526285/picdfa0abde605bee8112630a9fe241bb5b/600x800',
   );
-  await ctx.reply(
-    'You can control me by sending these button:',
-    Markup.inlineKeyboard([
-      Markup.button.callback('Get random quote', 'randomquote'),
-      Markup.button.callback('Get random image', 'randomimage'),
-    ]),
-  );
+  await sendCommandList(ctx, 'You can control me by sending these button:');
 });
 
 bot.action('randomquote', async (ctx) => {
@@ -25,6 +19,7 @@ bot.action('randomquote', async (ctx) => {
   });
 
   await ctx.sendMessage(`Anime: ${anime}\n\nCharacter: ${character}\n\nQuote: ${quote}`);
+  await sendCommandList(ctx);
 });
 
 bot.action('randomimage', async (ctx) => {
@@ -36,6 +31,7 @@ bot.action('randomimage', async (ctx) => {
   });
 
   await ctx.sendPhoto(url);
+  await sendCommandList(ctx);
 });
 
 const getDataFromApi = async ({ ctx, url }: { ctx: Context; url: string }) => {
@@ -44,6 +40,19 @@ const getDataFromApi = async ({ ctx, url }: { ctx: Context; url: string }) => {
   await ctx.deleteMessage(tempMessage.message_id);
 
   return res.data;
+};
+
+const sendCommandList = async (ctx: Context, text?: string) => {
+  await ctx.reply(
+    text ?? 'I can:',
+    Markup.inlineKeyboard(
+      [
+        Markup.button.callback('Get random quote', 'randomquote'),
+        Markup.button.callback('Get random image', 'randomimage'),
+      ],
+      { columns: 1 },
+    ),
+  );
 };
 
 bot.launch();
